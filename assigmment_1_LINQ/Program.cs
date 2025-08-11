459cda9e9b41a4e7c1cc56d81a317acc73f7de2d
mohamed gamal <mohamed.gamalabuy@gmail.com>
2025-08-11 04:31:17 +0300

    Add project files.

---
+++ assigmment_1_LINQ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace assigmment_1_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
